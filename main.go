2d50f59b94f55c7c59b10966d70fa33d9bf5405a
Niieq <nii.nmarley@gmail.com>
2020-05-24 15:06:09 +0000

    Add biography url

--- main.go
+++ main.go
@@ -10,12 +10,21 @@ import (
 func mainRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/biography", biographHandler)
+
+	staticFileDirectory := http.Dir("./static/")
+
+	staticFileHandler := http.StripPrefix("/static/", http.FileServer(staticFileDirectory))
+
+	r.PathPrefix("/static/").Handler(staticFileHandler).Methods("GET")
+
 	return r
 }
 
 func main() {
 
 	r := mainRouter()
+
 	http.ListenAndServe(":8080", r)
 }
 
@@ -24,4 +33,10 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/home.html"))
 	tmpl.Execute(w, nil)
 
+}
+
+func biographHandler(w http.ResponseWriter, r *http.Request) {
+
+	tmpl := template.Must(template.ParseFiles("templates/biography.html"))
+	tmpl.Execute(w, nil)
 }
