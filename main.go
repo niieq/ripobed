9d538fbe4e0845dc9b130d2d185ee78e4cd53e4b
Niieq <nii.nmarley@gmail.com>
2020-05-28 07:49:14 +0000

    Handle Image uploads" "

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/olahol/go-imageupload"
 )
 
 type Tribute struct {
@@ -79,19 +81,36 @@ func tributeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 
 		r.ParseForm()
+		img, err := imageupload.Process(r, "profile")
+
+		if err != nil {
+			panic(err)
+		}
+
+		thumb, err := imageupload.ThumbnailPNG(img, 60, 60)
+
+		if err != nil {
+			panic(err)
+		}
+
+		image_path := fmt.Sprintf("%d.png", time.Now().Unix())
+
+		thumb.Save("static/uploads/" + image_path)
+
 		name := r.Form["name"][0]
 		relationship := r.Form["relationship"][0]
 		tribute := r.Form["tribute"][0]
+		profile := image_path
 
-		var tributeRecord = Tribute{Name: name, Relationship: relationship, Tribute: tribute}
+		var tributeRecord = Tribute{Name: name, Relationship: relationship, Tribute: tribute, Profile: profile}
 
 		db.Create(&tributeRecord)
+
+		http.Redirect(w, r, "/tributes", http.StatusSeeOther)
 
 	} else {
 		var Tributes []Tribute
 		db.Find(&Tributes)
-
-		fmt.Println(Tributes)
 
 		tmpl := template.Must(template.ParseFiles("templates/tribute.html"))
 		tmpl.Execute(w, Tributes)
