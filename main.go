58d37fd808d666dab8bfda3fb75c13add7181f6b
Niieq <nii.nmarley@gmail.com>
2020-05-28 06:56:36 +0000

    Add db creation

--- main.go
+++ main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"net/http"
 
 	"github.com/gorilla/mux"
-	"net/http"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
+type Tribute struct {
+	gorm.Model
+	Name         string
+	Relationship string
+	Profile      string
+	Tribute      string
+}
+
+func initialMigration() {
+	db, err := gorm.Open("mysql", "root@/ripobed?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&Tribute{})
+}
 
 func mainRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler)
 	r.HandleFunc("/biography", biographHandler)
 	r.HandleFunc("/tributes", tributeHandler)
+
+	initialMigration()
 
 	staticFileDirectory := http.Dir("./static/")
 
@@ -44,6 +69,32 @@ func biographHandler(w http.ResponseWriter, r *http.Request) {
 
 func tributeHandler(w http.ResponseWriter, r *http.Request) {
 
-	tmpl := template.Must(template.ParseFiles("templates/tribute.html"))
-	tmpl.Execute(w, nil)
+	db, err := gorm.Open("mysql", "root@/ripobed?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	if r.Method == "POST" {
+
+		r.ParseForm()
+		name := r.Form["name"][0]
+		relationship := r.Form["relationship"][0]
+		tribute := r.Form["tribute"][0]
+
+		var tributeRecord = Tribute{Name: name, Relationship: relationship, Tribute: tribute}
+
+		db.Create(&tributeRecord)
+
+	} else {
+		var Tributes []Tribute
+		db.Find(&Tributes)
+
+		fmt.Println(Tributes)
+
+		tmpl := template.Must(template.ParseFiles("templates/tribute.html"))
+		tmpl.Execute(w, Tributes)
+	}
+
 }
